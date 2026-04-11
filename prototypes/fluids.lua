e65387a8f8f9a237b10521e7ac4a33fe32e74be5
SerotoninTheft <87555495+SerotoninTheft@users.noreply.github.com>
2026-04-10 20:52:21 -0700

    Work

--- prototypes/fluids.lua
+++ prototypes/fluids.lua
@@ -105,3 +105,78 @@ data:extend({
     150
   )
 })
+
+
+data:extend({
+
+  make_fluid("distilled-water", "a[fluid]-l[distilled-water]",
+    {r=0.3,g=0.6,b=1.0}, {r=0.6,g=0.8,b=1.0}, 15, 100),
+
+  make_fluid("very-clean-water", "a[fluid]-m[very-clean-water]",
+    {r=0.4,g=0.7,b=1.0}, {r=0.7,g=0.9,b=1.0}, 15, 100),
+
+  make_fluid("hydrogen", "a[fluid]-n[hydrogen]",
+    {r=0.85,g=0.9,b=1.0}, {r=1.0,g=1.0,b=1.0}, 15, 200),
+
+  make_fluid("oxygen", "a[fluid]-o[oxygen]",
+    {r=0.4,g=0.7,b=1.0}, {r=0.7,g=0.9,b=1.0}, 15, 200),
+
+  make_fluid("air", "a[fluid]-p[air]",
+    {r=0.7,g=0.8,b=0.9}, {r=0.9,g=0.95,b=1.0}, 15, 100),
+
+  make_fluid("dry-air", "a[fluid]-q[dry-air]",
+    {r=0.8,g=0.85,b=0.95}, {r=1.0,g=1.0,b=1.0}, 15, 100),
+
+  make_fluid("nitrogen", "a[fluid]-r[nitrogen]",
+    {r=0.5,g=0.6,b=0.9}, {r=0.7,g=0.8,b=1.0}, 15, 200),
+
+  make_fluid("argon", "a[fluid]-s[argon]",
+    {r=0.6,g=0.5,b=0.8}, {r=0.8,g=0.7,b=1.0}, 15, 200),
+
+  make_fluid("sulfur-dioxide", "a[fluid]-t[sulfur-dioxide]",
+    {r=0.8,g=0.8,b=0.2}, {r=1.0,g=1.0,b=0.4}, 25, 200),
+
+  make_fluid("sulfur-trioxide", "a[fluid]-u[sulfur-trioxide]",
+    {r=0.9,g=0.9,b=0.3}, {r=1.0,g=1.0,b=0.5}, 25, 200),
+
+  make_fluid("oleum", "a[fluid]-v[oleum]",
+    {r=0.6,g=0.5,b=0.2}, {r=0.8,g=0.7,b=0.3}, 25, 200),
+
+  make_fluid("ethylene", "a[fluid]-w[ethylene]",
+    {r=0.85,g=0.85,b=0.85}, {r=1.0,g=1.0,b=1.0}, 15, 200),
+
+  make_fluid("ethylene-oxide", "a[fluid]-w[ethylene-oxide]",
+    {r=0.85,g=0.85,b=0.85}, {r=1.0,g=1.0,b=1.0}, 15, 200),
+
+  make_fluid("butadiene", "a[fluid]-x[butadiene]",
+    {r=0.75,g=0.75,b=0.75}, {r=0.95,g=0.95,b=0.95}, 15, 200),
+
+  make_fluid("btx-aromatics", "a[fluid]-y[btx-aromatics]",
+    {r=0.6,g=0.4,b=0.2}, {r=0.8,g=0.6,b=0.3}, 25, 200),
+
+  make_fluid("benzene", "a[fluid]-z[benzene]",
+    {r=0.7,g=0.5,b=0.3}, {r=0.9,g=0.7,b=0.4}, 25, 200),
+
+  make_fluid("toluene", "a[fluid]-za[toluene]",
+    {r=0.65,g=0.45,b=0.25}, {r=0.85,g=0.65,b=0.35}, 25, 200),
+
+  make_fluid("xylene", "a[fluid]-zb[xylene]",
+    {r=0.6,g=0.4,b=0.2}, {r=0.8,g=0.6,b=0.3}, 25, 200),
+
+  make_fluid("ortho-xylene", "a[fluid]-zc[ortho-xylene]",
+    {r=0.55,g=0.35,b=0.2}, {r=0.75,g=0.55,b=0.3}, 25, 200),
+
+  make_fluid("meta-xylene", "a[fluid]-zd[meta-xylene]",
+    {r=0.5,g=0.3,b=0.2}, {r=0.7,g=0.5,b=0.3}, 25, 200),
+
+  make_fluid("para-xylene", "a[fluid]-ze[para-xylene]",
+    {r=0.65,g=0.4,b=0.25}, {r=0.85,g=0.6,b=0.35}, 25, 200),
+
+  make_fluid("metal-catalyst-solution", "a[fluid]-zf[metal-catalyst-solution]",
+    {r=0.7,g=0.7,b=0.5}, {r=0.9,g=0.9,b=0.7}, 25, 200),
+
+  make_fluid("cement-mix", "a[fluid]-zf[cement-mix]",
+    {r=0.7,g=0.7,b=0.5}, {r=0.9,g=0.9,b=0.7}, 25, 200),
+
+
+})
